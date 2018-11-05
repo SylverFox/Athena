2af28fbe4e2d2f191b3aa5f0aa2de6064757d6fc
Joris Diesvelt <jorisdiesvelt@gmail.com>
2018-11-06 00:27:44 +0100

    Migrated taskrunner to p-queue and added documentation

--- src/taskrunner.js
+++ src/taskrunner.js
@@ -1,27 +1,17 @@
 const {info, warn, startTimer} = require('winston')
-const async = require('async')
+const PQueue = require('p-queue')
 const config = require('config')
 
 const discovery = require('./discovery')
 const processing = require('./processing')
 
-let queue = async.queue(runTask, 1)
-
-function runTask(fn, cb) {
-	info('running: '+fn.name)
-	const timer = startTimer()
-	fn().then(() => {
-		timer.done('ended: '+fn.name)
-		cb()
-	}).catch(err => {
-		warn('failed: '+fn.name, err)
-		timer.done('ended: '+fn.name)
-		cb()
-	})
-}
+const queue = new PQueue({concurrency: 1})
 
 /** TASKS **/
 
+/**
+ * Discovers new hosts by pinging the entire range, look up their hostnames and listing their shares
+ */
 function discoverNewHosts() {
 	const startTime = Date.now()
 
@@ -33,6 +23,9 @@ function discoverNewHosts() {
 		.then(() => processing.insertNewScan('discoverNewHosts', startTime, Date.now() - startTime))
 }
 
+/**
+ * Pings all known hosts in the database and records their online status
+ */
 function pingKnownHosts() {
 	const startTime = Date.now()
 
@@ -42,6 +35,9 @@ function pingKnownHosts() {
 		.then(processing.insertNewScan('pingKnownHosts', startTime, Date.now() - startTime))
 }
 
+/**
+ * Indexes all know hosts and records it in the db
+ */
 function indexKnownHosts() {
 	const startTime = Date.now()
 
@@ -51,12 +47,16 @@ function indexKnownHosts() {
 		.then(() => processing.insertNewScan('indexKnownHosts', startTime, Date.now() - startTime))
 }
 
+/**
+ * Does post processing on indexed hosts, such as building keyword indexes
+ */
 function postProcessing() {
 	return processing.buildFileIndex()
 		.then(processing.buildDirectoryIndex)
 		.then(processing.buildKeywordIndex)
 }
 
+// TODO for later releases
 function indexStreamableContent() {
 	return processing.buildStreamableIndex()
 		.then(() => info('done indexing streamable content'))
@@ -65,29 +65,29 @@ function indexStreamableContent() {
 
 /** OUTSIDE FUNCTIONS **/
 
-exports.runFullDiscovery = function(defer) {
-	defer = defer || 0
-
-	setTimeout(() => {
-		queue.push(discoverNewHosts)
-		queue.push(pingKnownHosts)
-		queue.push(indexKnownHosts)
-		queue.push(postProcessing)	
-	}, defer)
+/**
+ * Adds a job to the queue that pings all known hosts and updates their status in the database
+ */
+exports.pingHosts = function() {
+	info('Running ping known hosts')
+	const timer = startTimer()
+	queue.add(pingKnownHosts)
+		.then(() => timer.done('Pinging known hosts done'))
+		.catch(err => warn('Pinging known hosts failed'))
 }
 
-exports.indexHosts = function(defer) {
-	defer = defer || 0
-
-	setTimeout(() => {
-		queue.push(indexKnownHosts)
-	}, defer)
-}
-
-exports.pingHosts = function(defer) {
-	defer = defer || 0
-
-	setTimeout(() => {
-		queue.push(pingKnownHosts)
-	}, defer)
+/**
+ * Runs a full discovery by finding new hosts, scanning who is online, indexing all their shared
+ * folders and doing some postprocesing
+ */
+exports.runFullDiscovery = function() {
+	info('Running full discovery')
+	const timer = startTimer()
+	queue.addAll([
+		discoverNewHosts,
+		pingKnownHosts,
+		indexKnownHosts,
+		postProcessing
+	]).then(() => timer.done('Full discovery done'))
+		.catch(err => warn('Full discovery failed', err.message))
 }
