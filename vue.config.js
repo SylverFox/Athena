3a0a52d0991a60074b928a6845b5d243e84516cb
SylverFox <jorisdiesvelt@gmail.com>
2019-06-29 22:07:35 +0200

    Fix public folder not picked up by Vue

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,25 @@
+const path = require('path')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+
 module.exports = {
   outputDir: 'client/dist/',
   devServer: {
     disableHostCheck: false
+  },
+  configureWebpack: {
+    plugins: [
+      new CopyWebpackPlugin([{
+        from: path.join(__dirname, 'client/public'),
+        to: path.join(__dirname, 'client/dist'),
+        toType: 'dir',
+        ignore: [ 'index.html' ]
+      }])
+    ]
+  },
+  chainWebpack: config => {
+    config.plugin('html').tap(args => {
+      args[0].template = path.join(__dirname,'client/public/index.html')
+      return args
+    })
   }
 }
